0bbe1273c862c8bb55473f5a959c503f9098ba08
Michael Jaskiewicz <michael.jaskiewicz@snapsheet.me>
2019-10-05 09:36:24 -0500

    fixing config

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jezykpolski::Application.config.secret_token = 'b46860e8a61f0ba56eb01ceb2c17fdbe0bdd9d6fbf5d0dfab589a8900f1ea8825b67ba3ac2d89da4ef26077464b0c34ac45c828b99fdf6670d3ac854afb7d523'
